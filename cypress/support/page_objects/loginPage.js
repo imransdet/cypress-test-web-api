b2369c004a5fec3b8f79cd7f5d412b913fe330a5
AL IMRAN <alimran@Imrans-MacBook.local>
2025-05-17 08:43:23 +0600

    Added login page

---
+++ cypress/support/page_objects/loginPage.js
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker'
+
+export class LoginPage{
+    clickOnDontHaveAccount(){
+        cy.get('.MuiGrid-root.MuiGrid-item.css-13i4rnv-MuiGrid-root').click();
+    }
+
+    enterFirstName(){
+        const firstName = faker.person.firstName()
+        cy.get('#firstName').type(firstName);
+    }
+    enterLastName(){
+        const lastName = faker.person.lastName()
+        cy.get('#lastName').type(lastName);
+    }
+
+    enterUsername(){
+        const userName = faker.internet.username();
+        cy.get('#username').type(userName);
+    }
+
+    enterPassword(){
+        cy.get('#password').type(password);
+    }
+
+    enterConfirmPassword(){
+        cy.get('#confirmPassword').type(password);
+    }
+
+}
+
+export const onLoginPage = new LoginPage();
