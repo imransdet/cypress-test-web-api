c8fa4bc120747dcf776a7ccb0a649ec0752b55fa
AL IMRAN <alimran@Imrans-MacBook.local>
2025-05-25 21:37:08 +0600

    updated config for report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    experimentalStudio: false
+    experimentalStudio: false,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'mochawesome-report',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: 'mm-dd-yyyy_HH-MM-ss'
+    },
   },
-})
+});
