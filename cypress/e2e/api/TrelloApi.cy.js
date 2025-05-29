21b882d53402712a6cbd5d0ea0d464ae47a9c555
AL IMRAN <alimran@Imrans-MacBook.local>
2025-05-29 08:11:19 +0600

    Recreate file safely with env vars

---
+++ cypress/e2e/api/TrelloApi.cy.js
@@ -0,0 +1,25 @@
+describe('Trello API Tests', () => {
+    const baseUrl = 'https://api.trello.com';
+    const trelloKey = Cypress.env('secretKey');
+    const trelloToken = Cypress.env('apiToken');
+
+    it('should fetch boards', () => {
+        cy.request({
+            method: 'GET',
+            url: `${baseUrl}/1/members/me/boards`,
+            qs: {
+                key: trelloKey,
+                token: trelloToken
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.be.an('array');
+            response.body.forEach(board => {
+                expect(board).to.have.property('id');
+                expect(board).to.have.property('name');
+            });
+        });
+
+    });
+
+});
